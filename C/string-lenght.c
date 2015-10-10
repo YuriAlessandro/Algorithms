f6b7c995985c070766d6b4f7fab57900ecc13d78
Elton Viana <eltonvs@outlook.com>
2015-10-10 17:02:55 -0300

    Improvements on for and fix indentation
    Removed break and if inside the for.

--- C/string-lenght.c
+++ C/string-lenght.c
@@ -2,19 +2,15 @@
 
 //A função "func_strlen" sempre irá calcular o tamanho da string quando chamada.
 
-int func_strlen(char *str){               
-   int i;
-   for(i=0; ;i++){
-      if(str[i] == 0) break;
-   }
-   return i;
+int func_strlen(char *str) {
+    for (int i = 0; str[i] != '\0'; i++);
+    return i;
 }
 
-int main()
-{
-   	char str[200];
-   	gets(str);
-   	printf("%i\n", func_strlen(str));
-   	
+int main() {
+    char str[200];
+    gets(str);
+    printf("%i\n", func_strlen(str));
+
     return 0;
 }
