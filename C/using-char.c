b363b36a4811540669ceb6afe27a638f82be4a58
Elton Viana <eltonvs@outlook.com>
2015-10-11 16:02:19 -0300

    Fix the buffer bug and code comments
    Added the function setbuf() and comment the code.

--- C/using-char.c
+++ C/using-char.c
@@ -12,14 +12,31 @@ int main()
     scanf("%s", name);
     //When using char, we do not use the "&" to indicate the address.
     
-    // printf("Put you nickname! ");
-    // gets(nick);
-    //"gets" is another way to get text. But be careful as it can cause memory overflow. Then we will use the following way:
+    printf("Put your nickname: ");
     
-    //fgets(nick,20,stdin);
-    //Then declaring as "fgets (name, size, stdin)".
-    //stdin reports that the input will come from the user's keyboard, and not from an external file.
+    setbuf(stdin, NULL);
+    /* setbuf() is a function to set the buffer.
+     * In this case, we need to clean the keyboard buffer.
+     * So, to do that, the 'stdin' (standard input) buffer
+     * was defined as NULL.
+     */
     
+    /* Now, we can call the function to get the nickname.
+     * gets(nick);
+     * gets() is another way to get text from user.
+     * But, your use isn't recommended because it can cause a memory overflow
+     * So, the right way to get text is the function fgets():
+     */
+    
+    fgets(nick, 20, stdin);
+    
+    /* This function takes three parameters.
+     * The first is the variable that will receive the text.
+     * The second is the maximum size of the input.
+     * And the last is where the text comes, in this case,
+     * we'll receive the text from standard input (keyboard, for example).
+     */
+
     printf("Hi %s, or %s!? How old are you??\n", name, nick);
     scanf("%i", &age);
     years = 2015 - age;
