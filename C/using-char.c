8b11416fa0d909819fc2c147a499f6b832b66c3a
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2015-10-11 13:45:33 -0300

    How work with char in C
    Existe um bug no código, futuramente será corrigido.

---
+++ C/using-char.c
@@ -0,0 +1,28 @@
+//Receiving text in C.
+
+#include <stdio.h>
+int main()
+{
+    char name[20], nick[20];
+    //To receive "words", use "char variable_name[size]"
+    
+    int age, years;
+    printf("What's your name?\n");
+    
+    scanf("%s", name);
+    //When using char, we do not use the "&" to indicate the address.
+    
+    // printf("Put you nickname! ");
+    // gets(nick);
+    //"gets" is another way to get text. But be careful as it can cause memory overflow. Then we will use the following way:
+    
+    //fgets(nick,20,stdin);
+    //Then declaring as "fgets (name, size, stdin)".
+    //stdin reports that the input will come from the user's keyboard, and not from an external file.
+    
+    printf("Hi %s, or %s!? How old are you??\n", name, nick);
+    scanf("%i", &age);
+    years = 2015 - (idade);
+    printf("Cool, %s. You born in %i!\n", name, years);
+    return 0;
+}
