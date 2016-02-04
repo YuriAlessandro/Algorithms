705ab541e672aafb32e41211c313299135695bac
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2016-02-04 12:01:13 -0300

    Update soma_pares.cpp

--- cpp/soma_pares.cpp
+++ cpp/soma_pares.cpp
@@ -1,5 +1,3 @@
-// This version are not tested yet
-
 #include <iostream>
 
 using namespace std;
@@ -7,15 +5,17 @@ using namespace std;
 int main(){
 
     int m, n, i, value = 0;
-    
+
     while (1){
+        cout << "Digite o valor de n\n";
         cin >> n;
+        cout << "\nDigite o valor de m\n";
         cin >> m;
-        if (n == 0 && m == 0) break; 
+        if (n == 0 && m == 0) break;
         for(i = 0; i < n; i++){
             value  += m + i;
         }
-        cout << value;
+        cout << "A soma dos n primeiros numeros a partir de m é: " << value << "\n";
     }
     return EXIT_SUCCESS;
 }
