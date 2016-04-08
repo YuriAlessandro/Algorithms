c985320e657561c8333ef55a3a4600ead3c13f7f
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2016-04-07 21:32:57 -0300

    Added files via upload

---
+++ Cpp/Functors/Mail/driver_message.cpp
@@ -0,0 +1,67 @@
+/**
+ * Exercício
+ * ---------
+ *
+ * [DONE] 1) Implementar message.cpp de acordo com a especificação de message.h.
+ * [DONE] 2) Imprimir as mensagens por ordem de
+ *    a) Remetente
+ *    b) Assunto
+ *    c) Data de envio
+ *    utilizando functors.
+ *
+ * [DONE] 3) Fazer o programa trabalhar com objetos mensagem criado com alocação
+ * dinâmica e ponteiros.
+ *
+ * [DONE] 4) Converter o programa do passo 2 para trabalhar com std::unique_ptr<>.
+ *
+ */
+#include <iostream>
+#include <algorithm>
+#include <memory>
+
+
+#include "message.h"
+
+int main( )
+{
+    std::unique_ptr< Message > pm1 (new Message( "selan@dimap.ufrn.br", "LP1", "201509011355", "jack@gmail.com", "jack.boy@hotmail.com", "false", "Aqui começa o corpo da mensagem\n Queremos voce por aqui.\n \n Ate breve!" ) );
+    std::unique_ptr< Message > pm2 (new Message( "andre@gmail.com", "Qualquer coisa", "201509101255" ));
+    std::unique_ptr< Message > pm3 (new Message( "silvia.mota@hotmail.com", "Pesquisa em conjunto", "201509101055" ));
+
+
+    std::vector< std::unique_ptr< Message > > ms;
+
+    ms.emplace_back( std::move( pm1 ) );
+    ms.emplace_back( std::move( pm2 ) );
+    ms.emplace_back( std::move( pm3 ) );
+
+    std::cout << ">>> Original messages: \n";
+    for ( auto const & e : ms )
+        std::cout << * e << "\n";
+    
+    sortMessage mySorterSubject( Message::HeaderField::SUBJECT );
+    sortMessage mySorterDate( Message::HeaderField::DATE );
+    sortMessage mySorterSender( Message::HeaderField::SENDER );
+
+    std::sort( ms.begin(), ms.end(), mySorterSubject );
+
+    std::cout << ">>> Sorted messages by Subject: \n";
+    for ( auto const & e : ms )
+        std::cout << *e << "\n";
+
+    std::sort( ms.begin(), ms.end(), mySorterDate );
+
+    std::cout << ">>> Sorted messages by Date: \n";
+    for ( auto const & e : ms )
+        std::cout << *e << "\n";
+
+
+    std::sort( ms.begin(), ms.end(), mySorterSender );
+
+    std::cout << ">>> Sorted messages by Sender: \n";
+    for ( auto const & e : ms )
+        std::cout << *e << "\n";
+
+    std::cout << ">>> Normal exiting....\n";
+    return EXIT_SUCCESS;
+}
