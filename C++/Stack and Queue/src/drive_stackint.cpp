5ea257461dc7586fc098040eb27487f26f07e19f
YuriAlessandro <yuri.alessandro@hotmail.com>
2016-04-13 20:09:58 -0300

    Arquivo de testes alterado para usar novas funções

--- C++/Stack and Queue/src/drive_stackint.cpp
+++ C++/Stack and Queue/src/drive_stackint.cpp
@@ -6,22 +6,21 @@ int main(){
 	
 	std::cout << ">>> Rodando StackInt:\n";
 
-	std::cout << "Declarando uma stack 'pilha1' com tamanho 50...\n";
-	StackInt<int> pilha1( 50 );
+	std::cout << "Declarando uma stack 'pilha1'\n";
+	StackInt<int> pilha1( 2 );
 	
 	bool cnt = pilha1.isEmpty();
 	(cnt == 1) ? std::cout << "A pilha está vazia...\n" : std::cout << "A pilha tem conteúdo...\n";
 	
 	std::cout << "Adicionando na pilha: 2\n";
-	std::cout << "Adicionando na pilha: 5\n";
-	std::cout << "Adicionando na pilha: 7\n";
-
 	pilha1.push( 2 );
+	std::cout << "Adicionando na pilha: 5\n";
 	pilha1.push( 5 );
+	std::cout << "Adicionando na pilha: 7\n";
 	pilha1.push( 7 );
 
 	cnt = pilha1.isEmpty();
-	 (cnt == 1) ? std::cout << "A pilha está vazia...\n" : std::cout << "A pilha tem conteúdo...\n";
+	(cnt == 1) ? std::cout << "A pilha está vazia...\n" : std::cout << "A pilha tem conteúdo...\n";
 
 	std::cout << "Elemento do topo: " << pilha1.top() << std::endl;
 
@@ -35,9 +34,18 @@ int main(){
 	pilha1.makeEmpty();
 
 	cnt = pilha1.isEmpty();
-	 (cnt == 1) ? std::cout << "A pilha está vazia...\n" : std::cout << "A pilha tem conteúdo...\n";
+	(cnt == 1) ? std::cout << "A pilha está vazia...\n" : std::cout << "A pilha tem conteúdo...\n";
 
-	 std::cout << ">>> Normal exiting...\n";
+	std::cout << "Adicionando na pilha: 100\n";
+	pilha1.push( 100 );
+	std::cout << "Adicionando na pilha: 0\n";
+	pilha1.push( 0 );
+	std::cout << "Adicionando na pilha: 23\n";
+	pilha1.push( 23 );
+
+	std::cout << "Elemento do topo: " << pilha1.top() << std::endl;
+
+	std::cout << ">>> Normal exiting...\n";
 
 	return EXIT_SUCCESS;
 }
