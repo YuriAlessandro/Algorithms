aef22d756ca79ef83abdbe6a7ddbcb8856e2d31a
YuriAlessandro <yuri.alessandro@hotmail.com>
2016-04-17 15:02:14 -0300

    Renamed 'First Program' to 'First Game'

---
+++ C++/SFML Learning/First Game/include/game_constants.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_CONSTANTS_H_
+#define _GAME_CONSTANTS_H_
+
+// Sizes
+const int SCRWIDTH = 600;
+const int SCRHEIGHT = 400;
+
+// Images
+#define BACKGROUND_IMG "assets/background.jpg"
+#define PERSON_IMG "assets/person.png"
+#define ENEMY_IMG "assets/enemy.png"
+#define LEVEL_IMG "assets/level.jpg"
+
+#endif
